aee1b9d8d2466a9224ad1e6b0b548c93dd9d670c
josephcleveland <jcleveland@ithaca.edu>
2018-07-13 15:45:08 -0400

    Added expressions to grammar

--- src/parser.h
+++ src/parser.h
@@ -17,17 +17,18 @@ void funcDef();
 //Parses ret list and returns return types in list
 std::vector<data_type> retList();
 std::vector<data_type> retListPrime();
-//Parses param list and returns var_defs in list
-std::vector<var_def> paramList();
-std::vector<var_def> paramListPrime();
-//Returns a var_def 
-var_def varDef();
+//Parses param list and returns var_decs in list
+std::vector<var_dec> paramList();
+std::vector<var_dec> paramListPrime();
+//Returns a var_dec 
+var_dec varDec();
 //returns type name, or reports error
 data_type typeName();
 
 //Other parser functions:
 void advance();
 void error(std::string message);
+std::vector<data_type> getTypesFromVarDecs(std::vector<var_dec> decs);
 }
 
 
