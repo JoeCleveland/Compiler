39f6915e96a4bb3bad646eb50a922de55b750352
josephcleveland <jcleveland@ithaca.edu>
2018-07-23 14:38:43 -0400

    Added struct for translation. TODO: change functions from strings to struct

--- src/translator.h
+++ src/translator.h
@@ -7,6 +7,13 @@
 #include <string>
 
 namespace translator {
+    enum inst_type { assign };
+    typedef struct instruction {
+        std::string dest;
+        std::string op1;
+        std::string op2;
+        inst_type type;
+    } inst_type;
     extern int tempVal;
     std::string getTempLabel();
     parser::exp_ret expressionLine(std::string in1, grammar_type op, std::string in2);
